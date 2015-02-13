c33def0a339906aeec8fe7cfbedb194e641a8dd2
Brendan ODowd <bodowd@worcester.edu>
2015-02-12 19:41:24 -0500

    Initial program with loop method, input error fixed

--- UpToFibonacci.c
+++ UpToFibonacci.c
@@ -4,6 +4,7 @@
 int input; //stores the inputted number
 const RANGE_BEGIN = 0; //the lowest number that will be accepted as input
 const RANGE_END = 35; //the largest number that will be accepted as input
+const FIB_SEED = 1; //the initial starting value of the Fibonacci sequence
 
 void check_input(); //function that will make sure user input is valid
 void fib_loop(); //calculates Fibonacci numbers with loops
@@ -11,8 +12,8 @@ void fib_loop(); //calculates Fibonacci numbers with loops
 int main () {
 printf("Please enter a number between 0 and 35: ");
 scanf("%d",&input); //read the input from user as an integer
-check_input(input);
-fib_loop(input);
+check_input(input); //check that the input is in bounds
+fib_loop(input); //run the Fibonacci output up to the given input
 
 return 0;
 }
@@ -26,9 +27,15 @@ void check_input(int a) { //checks to see if inputted number is with valid defin
 }
 
 void fib_loop(int bound) {
-	int first = 0;
-	int second = 1;
+	int first = FIB_SEED;
+	int second = FIB_SEED;
 	int result = 0;
+
+	if(bound >= 1) {
+		printf("%d ",first);
+		printf("%d ",second);
+	}
+
 	while (result < bound) {
 		result = first + second;
 		first = second;
