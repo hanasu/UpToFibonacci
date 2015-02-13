06c6e5886b54fb1c5ccb2b6c324f3843466ab23b
Brendan ODowd <bodowd@worcester.edu>
2015-02-12 19:49:16 -0500

    More comments and more descriptive instructions

--- UpToFibonacci.c
+++ UpToFibonacci.c
@@ -10,7 +10,7 @@ void check_input(); //function that will make sure user input is valid
 void fib_loop(); //calculates Fibonacci numbers with loops
 
 int main () {
-printf("Please enter a number between 0 and 35: ");
+printf("Please enter an integer between 0 and 35: ");
 scanf("%d",&input); //read the input from user as an integer
 check_input(input); //check that the input is in bounds
 fib_loop(input); //run the Fibonacci output up to the given input
@@ -27,20 +27,20 @@ void check_input(int a) { //checks to see if inputted number is with valid defin
 }
 
 void fib_loop(int bound) {
-	int first = FIB_SEED;
+	int first = FIB_SEED; //first and second sum required for Fibonacci sequence
 	int second = FIB_SEED;
-	int result = 0;
+	int result = 0; //where the new sum will be stored
 
-	if(bound >= 1) {
+	if(bound >= 1) { //initial values are larger than 0
 		printf("%d ",first);
 		printf("%d ",second);
 	}
 
-	while (result < bound) {
-		result = first + second;
-		first = second;
-		second = result;
-		if (result <= bound) { 
+	while (result < bound) { //if the last sum is not outside the bounds 
+		result = first + second; //store the sum of the previous two numbers
+		first = second; //the old second is now the new first
+		second = result; //the old result is now the new second
+		if (result <= bound) { //if the new sum is within the bounds, print it in sequence 
 			printf("%d ",result);
 		}
 	}
